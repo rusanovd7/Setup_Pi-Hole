e13b823bcb539fa0140eb28bd2a5ade3007eb6d0
Dimitar Dimitrov <38719695+piperka07@users.noreply.github.com>
2021-12-26 19:17:31 +0200

    Add files via upload

--- Setup_Pi-hole_vars.sh
+++ Setup_Pi-hole_vars.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ##########################
 # Enter configuration parameters below
 #
@@ -13,5 +15,24 @@ export DNSServer="1.1.1.1"
 export NewHostname=""
 # "yes" to disable IPv6 and "no" to leave it enabled
 export DisableIPv6="yes"
-# "yes" to disable bluetooth
+# "yes" to disable bluetooth and/or WiFi
 export DisableBluetooth="yes"
+export DisableWiFi="yes"
+# If set to "yes", the script will reduce the GPU memory to 16MB, in order to provide the maximum amount of RAM to the OS. Ignored if memory already set to a custom level. Set to "no" to keep the default VRAM amount.
+export MaxRAM="yes"
+## Go to https://github.com/jedisct1/dnscrypt-proxy/releases/latest and pick the link to the latest release for your architecture (linux_arm is for Raspbery Pi OS).
+export DNSCryptLink="https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.1.1/dnscrypt-proxy-linux_arm-2.1.1.tar.gz"
+#
+#
+######## WireGuard server setup ########
+# Ignore the variables below if you do not need the WireGuard container
+# Change to yes if you want to install the linuxserver.io WireGuard container
+export InstallWireGuard="no"
+# Change to your time zone
+export Timezone=Europe/Sofia
+# Your external IP address/domain
+export ServerURL=
+# Set the number of peers
+export Peers=
+# Set the WireGuard port
+export WGPort=51820
---
+++ Setup_WireGuard.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Install Docker
+if [[ "$(apt list 2>&1 | grep docker-ce)" == "" ]]; then
+    curl -sSL https://get.docker.com | sh
+    usermod -aG docker "${NewUserName}"
+fi
+
+systemctl restart docker
+
+# Install docker-compose
+#if [[ "$(apt list 2>&1 | grep docker-compose)" == "" ]]; then
+    apt install docker-compose -y
+#fi 
+
+# Create the WireGuard docker-compose.yml file and start the container
+if [ ! -d /opt/wireguard-server ]; then
+    mkdir /opt/wireguard-server
+fi
+cd /opt/wireguard-server || exit 1
+
+cat << EOF > /opt/wireguard-server/docker-compose.yml
+version: "2.1"
+services:
+  wireguard:
+    image: ghcr.io/linuxserver/wireguard
+    container_name: wireguard
+    cap_add:
+      - NET_ADMIN
+      - SYS_MODULE
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=${Timezone}
+      - SERVERURL=${ServerURL}
+      - SERVERPORT=${WGPort}
+      - PEERS=${Peers}
+# Currently Pi-Hole cannot be used as a DNS server for WG clients. WiP
+#     - PEERDNS=${IPAddress}
+      - PEERDNS=1.1.1.1
+      - INTERNAL_SUBNET=10.13.13.0
+      - ALLOWEDIPS=0.0.0.0/0
+    volumes:
+      - ./config:/config
+      - /lib/modules:/lib/modules
+    ports:
+      - ${WGPort}:51820/udp
+    sysctls:
+      - net.ipv4.conf.all.src_valid_mark=1
+#       - net.ipv4.ip_forward=1
+    restart: unless-stopped
+EOF
+
+docker-compose up -d
